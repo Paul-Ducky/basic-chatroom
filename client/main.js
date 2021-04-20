6b52f3bd5cc473ad9827862080e08735e9c8193a
Paul Sempels <paulsempels@gmail.com>
2021-04-20 14:24:12 +0200

    day2 done

--- client/main.js
+++ client/main.js
@@ -1,34 +1,62 @@
-let socket = io.connect();
-let target = document.getElementById('target');
-let onlineList = document.getElementById('onlineList');
+const socket = io.connect();
+const target = document.getElementById('target');
+const onlineList = document.getElementById('onlineList');
+const input = document.getElementById('chat-input');
 let usr = '';
+tinymce.init({
+    selector: "#chat-input",
+    skin: 'bootstrap',
+    plugins: "autoresize link lists emoticons",
+    toolbar:
+        "bold italic underline strikethrough | forecolor | numlist bullist | link blockquote emoticons",
+    menubar: false,
+    statusbar: false,
+    width: "100%",
+    toolbar_location: "bottom",
+    autoresize_bottom_margin: 0,
+    contextmenu: false,
+    setup: (ed) => {
+        editor = ed;
+    },
+});
+
+
+
 
 document.getElementById('usrname-btn').addEventListener('click',function(){
     usr = document.getElementById('usrname').value;
-    socket.emit('updateList',({socketID:socket.id, usr:usr}));
+    socket.emit('newUser',(usr));
     let elems = document.querySelectorAll('.usrname');
     for (const elem of elems){
         elem.remove(elem);
     }
 });
+
 document.getElementById('sendAll').addEventListener('click',function(){
-    let msg = document.getElementById('chat-input').value;
-    socket.emit('sendAll',({msg: msg, usr: usr}));
+    let msg = tinymce.get('chat-input').getContent();
+    console.log(msg);
+    socket.emit('sendAll',(msg));
     document.getElementById('chat-input').value = '';
 });
+
 document.getElementById('sendToMe').addEventListener('click',function(){
-    let msg = document.getElementById('chat-input').value;
-    socket.emit('sendToMe',({msg: msg, usr: usr}));
+    let msg = tinymce.get('chat-input').getContent();
+    socket.emit('sendToMe',(msg));
     document.getElementById('chat-input').value = '';
 });
 
 socket.on('displayMsg',(data) =>{
-    target.innerHTML += `<hr><br><h5>From: ${data.usr}</h5><br><p class="message">${data.msg}</p>`;
+    target.innerHTML += `<hr><br><h5>From: ${data.user}</h5><br>${data.message}`;
+});
+socket.on('userConnected', name => {
+    target.innerHTML += `<hr><br><h5>User: ${name} Connected!</h5><br><p class="message">Say hi!</p>`;
+});
+socket.on('userLeft', name => {
+    target.innerHTML += `<hr><br><h5>User: ${name} Disconnected!</h5><br><p class="message">Bye!</p>`;
 });
 socket.on('updateList',(users)=>{
     onlineList.innerHTML ='';
-    for (const user of users)
-    {
-        onlineList.innerHTML += `<ul><span class="dot"></span>${user.usr}</ul>`;
-    }
+    Object.entries(users).forEach(([key, value]) => {
+        onlineList.innerHTML += `<ul><span class="dot"></span>${value}</ul>`;
+    });
 });
--- server/server.js
+++ server/server.js
@@ -1,46 +1,47 @@
 const express = require('express');
-const http = require('http');
 const app = express();
+const http = require('http');
 const clientPath = `${__dirname}/../client`;
 const PORT = 8080;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
 const io = require('socket.io')(server);
+const onlineUsers = {}
+let counter = 0;
 
 server.listen(PORT,()=>{
     console.log("server running on "+PORT);
 });
-
-const onlineUsers = []
-
-
-let counter = 0;
 io.on('connection', (socket) =>{
+    io.emit("updateList",(onlineUsers));
     counter++;
     console.log("someone connected");
     console.log(counter + " connections.");
-    socket.on('updateList',(data) =>{
-        onlineUsers.push(data);
-        io.emit("updateList",(onlineUsers))
+    socket.on('newUser',name =>{
+        onlineUsers[socket.id] = name;
+        socket.broadcast.emit('userConnected', name);
+        io.emit("updateList",(onlineUsers));
     });
     socket.on('disconnect',() =>{
         counter--;
-        let i = 0;
-        for(const user of onlineUsers){
-            console.log(user.socketID);
-            if(user.socketID === socket.id){
-                onlineUsers.splice(i,1);
-            }
-            i++;
-        }
+        //let i = 0;
+        //for(const user of onlineUsers){
+        //    console.log(user.socketID);
+        //    if(user.socketID === socket.id){
+        //        onlineUsers.splice(i,1);
+        //    }
+        //    i++;
+        //}
+        io.emit('userLeft',onlineUsers[socket.id]);
+        delete onlineUsers[socket.id];
         console.log(onlineUsers);
         console.log(counter + " connections.");
         io.emit("updateList",(onlineUsers));
     });
-    socket.on('sendAll',(data) =>{
-        io.emit("displayMsg",(data));
+    socket.on('sendAll',(message) =>{
+        io.emit("displayMsg",({message: message, user: onlineUsers[socket.id]}));
     });
-    socket.on('sendToMe',(data) =>{
-        socket.emit("displayMsg",(data));
+    socket.on('sendToMe',(message) =>{
+        socket.emit("displayMsg",({message: message, user: onlineUsers[socket.id]}));
     });
 });
